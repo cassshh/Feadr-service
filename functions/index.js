cf253cc30ddc1e54d930442086d59cf3b1c63acc
cassshh <casvd@hotmail.com>
2017-05-21 15:24:32 +0200

    create post listener

--- functions/index.js
+++ functions/index.js
@@ -9,3 +9,32 @@ admin.initializeApp(functions.config().firebase);
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createPost = functions.database.ref('/queue_create_post/{postUid}').onWrite(event => {
+    console.log('Create Post called')
+    const post = event.data.val();
+    const postUid = event.params.postUid;
+
+    if (!post) {
+        return console.log('Post removed from queue');
+    }
+
+    console.log(post);
+
+    const createOverviewItem = admin.database().ref('/overview/$postUid').set({
+        user_uid: post.user_uid,
+        username: post.username,
+        title: post.title,
+        thumbnail: post.thumbnail,
+        time: post.time,
+        location: post.location
+    });
+    const createPostItem = admin.database().ref('/posts/$postUid').set(post);
+
+    return Promise.all([createOverviewItem, createPostItem]).then(results => {
+        console.log('Post has been created')
+        return event.ref().remove().then(result => {
+            console.log('Removed from queue');
+        });
+    });
+});
