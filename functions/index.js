6abafec70820f737f8139ffcdbf363cab9b90709
cassshh <casvd@hotmail.com>
2017-05-21 19:50:55 +0200

    Refactoring

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,14 @@
-const functions = require('firebase-functions');
+'use strict';
 
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const queue = require('./queue');
 
-exports.createPost = functions.database.ref('/queue_create_post/{postUid}').onWrite(event => {
-    console.log('Create Post called')
-    const post = event.data.val();
-    const postUid = event.params.postUid;
+const PostCreated = functions.database.ref('/queue/posts/{postUid}')
+    .onWrite(queue.processNewPost);
 
-    if (!post) {
-        return console.log('Post removed from queue');
-    }
-
-    console.log(post);
-
-    const createOverviewItem = admin.database().ref(`/overview/${postUid}`).set({
-        user_uid: post.user_uid,
-        username: post.username,
-        title: post.title,
-        thumbnail: post.thumbnail,
-        time: post.time,
-        location: post.location
-    });
-    const createPostItem = admin.database().ref(`/posts/${postUid}`).set(post);
-
-    return Promise.all([createOverviewItem, createPostItem]).then(results => {
-        console.log('Post has been created')
-        return event.data.ref.remove().then(result => {
-            console.log('Removed from queue');
-        });
-    });
-});
+module.exports = {
+    PostCreated: PostCreated
+};
---
+++ functions/queue.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function processNewPost(evt = DeltaSnapShot) {
+    console.log('Processing new post');
+    const post = evt.data.val();
+    const postUid = evt.param.postUid;
+
+    if (!post) {
+        return console.log('Post removed from queue');
+    }
+
+    const overviewObject = {
+        title: post.title,
+        location: post.location,
+        time: post.time,
+        user_uid: post.user_uid,
+        username: post.username,
+    }
+    if (post.content) {
+        for (let section of Object.keys(post.content)) {
+            if (post.console[section].images) {
+                // Make first image the thumbnail
+                overviewObject.thumbnail = Object.values(post.content[section].images)[0];
+                break;
+            }
+        }
+    }
+
+    const overviewPromise = admin.database().ref(`/overview/${postUid}`).set(overviewObject);
+    const postPromise = admin.database().ref(`/posts/${postUid}`).set(post);
+
+    return Promise.all([overviewPromise, postPromise]).then(() => {
+        console.log('Post has been processed');
+        return event.data.ref.remove();
+    });
+}
+
+module.exports = {
+    processNewPost: processNewPost
+};
