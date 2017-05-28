ca4a03b339b03b5c3c4acf02b9f6f08d80bdf627
cassshh <casvd@hotmail.com>
2017-05-28 14:56:52 +0200

    Process location data

--- functions/queue.js
+++ functions/queue.js
@@ -1,44 +1,80 @@
 'use strict';
 
-function processNewPost(evt = DeltaSnapShot) {
-    console.log('Processing new post');
-    const post = evt.data.val();
-    const postUid = evt.params.postUid;
+function processNewPost(evt) {
+  console.log('Processing new post');
+  const post = evt.data.val();
+  const postUid = evt.params.postUid;
+  const promises = [];
 
-    if (!post) {
-        return console.log('Post removed from queue');
-    }
+  // Post is removed
+  if (!post) {
+    return console.log('Post removed from queue');
+  }
 
-    if (post.time) {
-        post.time = new Date(post.time).toISOString();
-    }
+  // Set time format
+  if (post.time) {
+    post.time = new Date(post.time).toISOString();
+  }
 
-    const overviewObject = {
-        title: post.title,
-        location: post.location,
-        time: post.time,
-        user_uid: post.user_uid,
-        username: post.username,
-    }
-    if (post.content) {
-        for (let section of Object.keys(post.content)) {
-            if (post.content[section].images) {
-                // Make first image the thumbnail
-                overviewObject.thumbnail = post.content[section].images[Object.keys(post.content[section].images)[0]];
-                break;
-            }
+  /*
+   route: street,
+   locality: city,
+   administrative_area_level_1: state,
+   administrative_area_level_2: county,
+   country: country,
+   postal_code: postal code
+   */
+  const searchLoc = ['route', 'locality', 'administrative_area_level_1', 'administrative_area_level_2', 'country', 'postal_code'];
+
+  // Set location
+  let location = {};
+  if (post.location.address_components) {
+    for (const address of post.location.address_components) {
+      for (const type of address.types) {
+        if (searchLoc.includes(type)) {
+          location[type] = {
+            long_name: address.long_name,
+            short_name: address.short_name,
+          };
         }
+      }
     }
+  }
+  post.location = location;
 
-    const overviewPromise = evt.data.adminRef.root.child(`/overview/${postUid}`).set(overviewObject);
-    const postPromise = evt.data.adminRef.root.child(`/posts/${postUid}`).set(post);
+  // Set overview object
+  const overviewObject = {
+    title: post.title,
+    location: post.location,
+    time: post.time,
+    user_uid: post.user_uid,
+    username: post.username,
+  };
 
-    return Promise.all([overviewPromise, postPromise]).then(() => {
-        console.log('Post has been processed');
-        return evt.data.ref.remove();
-    });
+  // Set thumbnail if image(s)
+  if (post.content) {
+    for (const section of Object.keys(post.content)) {
+      if (post.content[section].images) {
+        // Make first image the thumbnail
+        overviewObject.thumbnail = post.content[section].images[Object.keys(post.content[section].images)[0]];
+        break;
+      }
+    }
+  }
+
+  for (const type in location) {
+    promises.push(evt.data.adminRef.root.child(`/location/${type}/${location[type].long_name}/posts/${postUid}`).set(postUid));
+  }
+
+  promises.push(evt.data.adminRef.root.child(`/overview/${postUid}`).set(overviewObject));
+  promises.push(evt.data.adminRef.root.child(`/posts/${postUid}`).set(post));
+
+  return Promise.all(promises).then(() => {
+    console.log('Post has been processed');
+    return evt.data.ref.remove();
+  });
 }
 
 module.exports = {
-    processNewPost: processNewPost
+  processNewPost: processNewPost
 };
